ebfddfb99197cfd2af49ca5e79242f98523e8cfe
kev <kevin.15.09@hotmail.com>
2024-07-22 15:39:36 -0500

    spring heroku app config

---
+++ src/main/java/com/inverse/restapi/springdeployfirst/SpringDeployFirstApplication.java
@@ -0,0 +1,13 @@
+package com.inverse.restapi.springdeployfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeployFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeployFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inverse/restapi/springdeployfirst/SpringDeployFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inverse.restapi.springdeployfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
